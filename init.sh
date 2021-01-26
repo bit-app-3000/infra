1169153bd53e90392eb1ec6c48a2d38e986e69e7
bt <>
2021-01-26 16:26:34 +0200

    wip

--- init.sh
+++ init.sh
@@ -1,1 +1,9 @@
 argocd proj create infra -d https://kubernetes.default.svc,infra -s https://github.com/mmd-app/infra.git
+
+
+
+argocd app create infra --repo https://github.com/mmd-app/infra.git --path gitops --dest-server https://kubernetes.default.svc --dest-namespace infra
+
+
+argocd app set infra --project infra
+
